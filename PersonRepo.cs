cd47062dce3dedeff2be39446248823d8b5168cc
unknown <jimpidas>
2021-06-08 23:25:04 +0530

    [Jyosmita]Added: retrieving person belonging to a city or state

--- PersonRepo.cs
+++ PersonRepo.cs
@@ -49,6 +49,50 @@ namespace AddressBook_ADO
                 this.connection.Close();
             }
             return false;
-        } 
+        }
+
+        public void GetAllEmployee()
+        {
+            try
+            {
+                PersonModel personModel = new PersonModel();
+                using (this.connection)
+                {
+                    
+                    SqlCommand cmd = new SqlCommand(@"select * from AddressBookDB where state='assam'", this.connection);
+                    this.connection.Open();
+                    SqlDataReader dr = cmd.ExecuteReader();
+                    if (dr.HasRows)
+                    {
+                        while (dr.Read())
+                        {
+                            personModel.FirstName = dr.GetString(0);
+                            personModel.LastName = dr.GetString(1);
+                            personModel.Address = dr.GetString(2);
+                            personModel.PhoneNumber = dr.GetString(6);
+                            personModel.zip = dr.GetInt32(5);
+                            personModel.City = dr.GetString(3);
+                            personModel.State = dr.GetString(4);
+                            personModel.AddressBookName = dr.GetString(8);
+                            personModel.Email = dr.GetString(7);
+                            personModel.Type = dr.GetString(9);
+                            
+                            System.Console.WriteLine(personModel.FirstName + " " + personModel.LastName + " " + personModel.Address + " " + personModel.City + " " + personModel.zip + " " + personModel.State + " " + personModel.PhoneNumber + " " + personModel.AddressBookName + " " + personModel.Type);
+                            System.Console.WriteLine("\n");
+
+                        }
+                    }
+                    else
+                    {
+                        System.Console.WriteLine("No data found");
+
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                System.Console.WriteLine(e.Message);
+            }
+        }
     }
 }
