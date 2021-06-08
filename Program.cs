c1e13617713460068dc8acb2d8a86c75b8d999c8
unknown <jimpidas>
2021-06-08 22:42:39 +0530

    [Jyosmita]: Added code to connect database

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace AddressBook_ADO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Payroll!");
+            PersonRepo repo = new PersonRepo();
+            PersonModel employee = new PersonModel();
+            employee.FirstName = "Mukti";
+            employee.LastName = "hk";
+            employee.Address = "House no 22";
+            employee.PhoneNumber = "6302907678";
+            employee.zip = 894523;
+            employee.City = "Tsk";
+            employee.State = "Tripura";
+            employee.Email = "muktihk@gmail.com";
+            employee.AddressBookName ="book4";
+            employee.Type ="Family";
+
+            if (repo.AddEmployee(employee))
+            Console.WriteLine("Records added successfully");
+            Console.ReadKey();
+        }
+    }
+}
